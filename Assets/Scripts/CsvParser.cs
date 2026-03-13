32eb962f53e7666a702b8e282bfeabadfa321037
NuOrpheus <160181551+NuOrpheus@users.noreply.github.com>
2026-03-13 17:55:37 +0000

    moderate bug fixes to scripts

--- Assets/Scripts/CsvParser.cs
+++ Assets/Scripts/CsvParser.cs
@@ -3,14 +3,13 @@ using System.Collections;
 using System.Collections.Generic;
 public class CsvParser : MonoBehaviour {
     public string tmp;
-    public string InputDialogue = "inputDialogue";
-    public string InputSpeaker = "inputSpeaker";
-    public string InputChoice1 = "inputChoice1";
-    public string InputChoice2 = "inputChoice2";
-    public string InputExit1 = "start1";
-    public string InputExit2 = "start1";
+    public string InputDialogue;
+    public string InputSpeaker;
+    public string InputChoice1;
+    public string InputChoice2;
+    public string InputExit1;
+    public string InputExit2;
     public Dictionary<string, Dictionary<string, string>> storage = new Dictionary<string, Dictionary<string, string>>();
-    //Debug.Log(InputExits[0]);
     private void AddOuterSection(string section) {
         storage.Add(section, new Dictionary<string, string>());
     }
@@ -18,18 +17,23 @@ public class CsvParser : MonoBehaviour {
         storage[section].Add(value1, value2);
     }
     public void nextSection(string section) {
-        InputSpeaker = storage[section]["speaker"];
-        InputDialogue = storage[section]["dialogue"];
-        InputChoice1 = storage[section]["choice1"];
-        InputChoice2 = storage[section]["choice2"];
-        InputExit1 = storage[section]["exit1"];
-        InputExit2 = storage[section]["exit2"];
-        
-        //return(string[storage[section]["exit1"],storage[section]["exit2"]]);
+        try {InputSpeaker = storage[section]["speaker"];} 
+        catch {InputSpeaker = "";}
+        try {InputDialogue = storage[section]["dialogue"];}
+        catch{InputDialogue = "";}
+        try {InputChoice1 = storage[section]["choice1"];}
+        catch {InputChoice1 = "";}
+        try {InputChoice2 = storage[section]["choice2"];}
+        catch {InputChoice1 = "";}
+        try {InputExit1 = storage[section]["exit1"];}
+        catch {InputExit1 = "";}
+        try {InputExit2 = storage[section]["exit2"];}
+        catch {InputExit2 = "";}
     }
-    void Start()
+    void Awake()
     {
-        //https://discussions.unity.com/t/how-to-read-a-dataset-from-a-csv/783544/6 start copied code
+        //https://discussions.unity.com/t/how-to-read-a-dataset-from-a-csv/783544/6 
+        // start copied code
         TextAsset dataset = Resources.Load<TextAsset>("dialogue");
         string[] lines = dataset.text.Split("\n");
         List<List<string>> lists = new List<List<string>>();
@@ -38,37 +42,35 @@ public class CsvParser : MonoBehaviour {
             List<string> list = new List<string>(data);
             lists.Add(list);
         }
-        /////////// end copied code
-        /*AddOuterSection("start1");
-        AddInnerSection("start1","speaker","???");
-        AddInnerSection("start1","dialogue","first dialogue");
-        AddInnerSection("start1","choice1","first choice");
-        AddInnerSection("start1","choice2","second choice");
-        AddInnerSection("start1","exit1","start1A");
-        AddInnerSection("start1","exit2","start1B");
-        foreach(var z in storage) {
-            Debug.Log(z.Key);
-            //Debug.Log(z.Value);
-            foreach(var y in z.Value) {
-                Debug.Log(y.Key +y.Value);
-                //Debug.Log(y.Value);
-            }
-        }*/
-        //Debug.Log(storage["start1"]["speaker"]);
+        // end copied code
         for (int col = 1; col < lists.Count-1; col++) {
-            for (int row = 0; row < /*lists.Count*/7; row++) {
-                try {
-                    if (row == 0) {
+            for (int row = 0; row < 7; row++) {
+                switch (row) {
+                    case 0:
                         AddOuterSection(lists[col][row]);
                         tmp = lists[col][row];
-                    } else if (row == 1) AddInnerSection(tmp, "speaker", lists[col][row]);
-                    else if (row == 2) AddInnerSection(tmp, "dialogue", lists[col][row]);
-                    else if (row == 3) AddInnerSection(tmp, "choice1", lists[col][row]);
-                    else if (row == 4) AddInnerSection(tmp, "choice2", lists[col][row]);
-                    else if (row == 5) AddInnerSection(tmp, "exit1", lists[col][row]);
-                    else if (row == 6) AddInnerSection(tmp, "exit2", lists[col][row]);
-                } catch {}
+                        break;
+                    case 1: 
+                        AddInnerSection(tmp, "speaker", lists[col][row]);
+                        break;
+                    case 2: 
+                        AddInnerSection(tmp, "dialogue", lists[col][row]);
+                        break;
+                    case 3: 
+                        AddInnerSection(tmp, "choice1", lists[col][row]);
+                        break;
+                    case 4: 
+                        AddInnerSection(tmp, "choice2", lists[col][row]);
+                        break;
+                    case 5: 
+                        AddInnerSection(tmp, "exit1", lists[col][row]);
+                        break;
+                    case 6:
+                        AddInnerSection(tmp, "exit2", lists[col][row]);
+                        break;
+                }   
             }
         }
+        nextSection("start");
     }
 }
--- Assets/Scripts/DialogueManager.cs
+++ Assets/Scripts/DialogueManager.cs
@@ -2,38 +2,40 @@ using UnityEngine;
 using TMPro;
 using System.Collections;
 using System.Collections.Generic;
-public class DialogueManager : MonoBehaviour
-{
+public class DialogueManager : MonoBehaviour {
+    public static DialogueManager Instance;
     [SerializeField] public GameObject choicesPanel;
     [SerializeField] public TMP_Text currentChoice1;
     [SerializeField] public TMP_Text currentChoice2;
     [SerializeField] public TMP_Text currentDialogue;
-    [SerializeField] public TMP_Text currentSpeaker ;
+    [SerializeField] public TMP_Text currentSpeaker;
     private float x = 0f;
     public CsvParser csvParser;
     private void Start() {
         csvParser = GetComponent<CsvParser>();
-        ChoicesActivation(false);
+        choicesPanel.SetActive(false);
+        ChangeText("start");
     }
-    public void Update() {
+    public void OnClickChoice1() {
+        ChangeText(csvParser.InputExit1);
+    }
+    public void OnClickChoice2() {
+        ChangeText(csvParser.InputExit2);
+    }
+    private void Update() {
         x += Time.deltaTime;
-        if (Input.GetButton("Jump")) {
-            if (x > 0.2) {
-                try {
-                ChangeText(csvParser.InputExit1);
-                x = 0;
-                } catch {}
-            }
+        if (Input.GetButton("Jump") && (x > 0.2)) {
+            if (!choicesPanel.activeSelf) ChangeText(csvParser.InputExit1);   
+            x = 0;
         }
     }
     public void ChangeText(string section) {
-        currentDialogue.text = csvParser.InputDialogue;
+        csvParser.nextSection(section);
+        if (string.IsNullOrEmpty(csvParser.InputChoice2)) choicesPanel.SetActive(false);
+        else choicesPanel.SetActive(true);
         currentSpeaker.text = csvParser.InputSpeaker;  
+        currentDialogue.text = csvParser.InputDialogue;
         currentChoice1.text = csvParser.InputChoice1;
         currentChoice2.text = csvParser.InputChoice2;
-        csvParser.nextSection(section);
     }
-    public void ChoicesActivation(bool x) {
-        choicesPanel.SetActive(x);
-    }   
 }
