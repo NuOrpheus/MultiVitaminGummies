96679be3331067e5ef7298a2b32a757c48dbeef8
NuOrpheus <160181551+NuOrpheus@users.noreply.github.com>
2026-04-05 17:13:40 +0100

    improved button animation integration

--- Assets/Scripts/DialogueManager.cs
+++ Assets/Scripts/DialogueManager.cs
@@ -5,13 +5,14 @@ using System.Collections.Generic;
 public class DialogueManager : MonoBehaviour {
     public static DialogueManager Instance;
     [SerializeField] private GameObject choicesPanel;
-    [SerializeField] private GameObject choiceImage1;
-    [SerializeField] private GameObject choiceImage2;
+    [SerializeField] private GameObject choiceObject1;
+    [SerializeField] private GameObject choiceObject2;
     [SerializeField] private TMP_Text currentChoice1;
     [SerializeField] private TMP_Text currentChoice2;
     [SerializeField] private TMP_Text currentDialogue;
     [SerializeField] private TMP_Text currentSpeaker;
     private float x = 0f;
+    private Sprite choiceImage1;
     private bool wait = false;
     private CsvParser csvParser;
     [SerializeField] private AudioManager audioManager;
@@ -34,6 +35,7 @@ public class DialogueManager : MonoBehaviour {
     }
     public void OnClickRestart() {
         audioManager.PlayButtonSfx();
+        choicesPanel.SetActive(false);
         ChangeText("start", 0);
     }
     private void Update() {
@@ -46,21 +48,25 @@ public class DialogueManager : MonoBehaviour {
     public void ChangeText(string section, int choice) {
         csvParser.nextSection(section);
         if (string.IsNullOrEmpty(csvParser.InputChoice2)) {
-            wait = true;
             switch (choice) {
                 case 1:
-                    choiceImage2.SetActive(false);
+                    wait = true;
+                    choiceObject2.transform.localScale = new Vector3(0,1,1);
+                    Invoke(nameof(choicesOff), 0.3f);
                     break;
                 case 2:
-                    choiceImage1.SetActive(false);
+                    wait = true;
+                    choiceObject1.transform.localScale = new Vector3(0,1,1);
+                    Invoke(nameof(choicesOff), 0.3f);
                     break;
             }
-            Invoke(nameof(choicesOff), 0.1f);
         }
         else choicesPanel.SetActive(true);
         currentSpeaker.text = csvParser.InputSpeaker;  
         currentDialogue.text = csvParser.InputDialogue;
         if (!wait) {
+            choiceObject1.transform.localScale = new Vector3(1,1,1);
+            choiceObject2.transform.localScale = new Vector3(1,1,1);
             currentChoice1.text = csvParser.InputChoice1;
             currentChoice2.text = csvParser.InputChoice2;
         }
