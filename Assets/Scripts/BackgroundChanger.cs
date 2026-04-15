bf4ca33a10f42e7d322f3f59b01c31a8adfc8799
NuOrpheus <160181551+NuOrpheus@users.noreply.github.com>
2026-04-15 16:10:22 +0100

    continued development of changable background script

--- Assets/Scripts/BackgroundChanger.cs
+++ Assets/Scripts/BackgroundChanger.cs
@@ -2,11 +2,11 @@ using UnityEngine;
 
 public class backgroundChanger : MonoBehaviour
 {
-    public GameObject background;
+    [SerializeField] public GameObject background;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+       SpriteRenderer background = background.GetComponent<SpriteRenderer>(); 
     }
 
     // Update is called once per frame
